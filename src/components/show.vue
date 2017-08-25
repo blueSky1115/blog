0f706b162993c629ccdb87103f3e802f037ed36b
blueSky1115 <hwn199138@126.com>
2017-08-26 00:53:53 +0800

    dabao

--- src/components/show.vue
+++ src/components/show.vue
@@ -7,20 +7,71 @@
     <div id="">
       <comheader></comheader>
       <div class="neirong5">
-        <h1>Project Show</h1>
+        <h1>
+          Project Show<br/>
+          部分项目展示
+        </h1>
         <ul class="list" id="list">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <a target="_blank" href="https://bluesky1115.github.io/tiger/index.html">
+            <li>
+              <img src="../assets/img/pizza.png" alt="">
+            </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/dell/dell.html">
+          <li>
+            <img src="../assets/img/dell.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/ifs/index.html">
+          <li>
+            <img src="../assets/img/ifs.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/cocacola/homepage.html">
+          <li>
+            <img src="../assets/img/kekoukele.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/aigan/index.html">
+          <li>
+            <img src="../assets/img/aigan.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="http://www.isantai.com/">
+            <li>
+              <img src="../assets/img/santai.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/Basics/basics.html">
+          <li>
+            <img src="../assets/img/jiazhuang.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/car01/index.html">
+          <li>
+            <img src="../assets/img/car.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/wanbang/index.html">
+          <li>
+            <img src="../assets/img/wangbang.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/kele/index.html">
+          <li>
+            <img src="../assets/img/kele.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/36kr/index.html">
+          <li>
+            <img src="../assets/img/36k.png" alt="">
+          </li>
+          </a>
+          <a target="_blank" href="https://bluesky1115.github.io/lookgame/index.html">
+          <li>
+            <img src="../assets/img/look.png" alt="">
+          </li>
+          </a>
         </ul>
       </div>
       <comfooter></comfooter>
@@ -58,16 +109,20 @@
   }
   .neirong5 .list{
     width: 850px;
-    height: 1000px;
+    height: 1200px;
     /*border: 1px solid red;*/
     margin: 0 auto;
   }
   .neirong5 .list li{
-    width: 260px;
-    height: 260px;
+    width: 200px;
+    height: 200px;
     border: 1px solid blue;
     float: left;
-    margin: 10px;
+    margin: 40px;
     overflow: hidden;
+  }
+  .neirong5 .list li img{
+    width: 200px;
+    height: 200px;
   }
 </style>
