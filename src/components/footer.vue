c594f00bc1bdc29fc4c0f723229ee7878c2243aa
blueSky1115 <hwn199138@126.com>
2017-08-29 23:32:30 +0800

    www
    www

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -3,8 +3,8 @@
 		<div class="footone">
 			<img src="../assets/img/erweima.png" alt="">
 			<ul class="listtwo">
-				<li>联系QQ:55498238</li>
-				<li>联系邮箱:liaochen55498238@qq.com</li>
+				<li>QQ:55498238</li>
+				<li>邮箱:liaochen55498238@qq.com</li>
 				<li>联系电话:18683934731</li>
 				<li>注册号:暂时未注册.com.cn</li>
 			</ul>
